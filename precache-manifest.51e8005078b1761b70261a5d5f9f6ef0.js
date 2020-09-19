d7b667c5816798c056a75bd01061f4e04ce9d117
douglas <d.waligura@cinq.com.br>
2020-09-19 14:25:52 -0300

    Updates

---
+++ precache-manifest.51e8005078b1761b70261a5d5f9f6ef0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5a77d387ee78a2ca26aa2d14fd45c98",
+    "url": "/react-ts-flappy-bird/index.html"
+  },
+  {
+    "revision": "4b5b4f63b2d5caebffa2",
+    "url": "/react-ts-flappy-bird/static/js/2.82bf5a9f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-ts-flappy-bird/static/js/2.82bf5a9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43dd1a65d910a7f968c5",
+    "url": "/react-ts-flappy-bird/static/js/main.ce070f94.chunk.js"
+  },
+  {
+    "revision": "2acfec413e6db736e335",
+    "url": "/react-ts-flappy-bird/static/js/runtime-main.d595ace3.js"
+  },
+  {
+    "revision": "f97dc14b469e7aba06579207a33b1946",
+    "url": "/react-ts-flappy-bird/static/media/bg.f97dc14b.png"
+  },
+  {
+    "revision": "cab12c378520d317a8ec94585fa4af5f",
+    "url": "/react-ts-flappy-bird/static/media/bird.cab12c37.png"
+  },
+  {
+    "revision": "6ecbd5373509a49868bb5c373f34d881",
+    "url": "/react-ts-flappy-bird/static/media/fg.6ecbd537.png"
+  },
+  {
+    "revision": "07cd2335e8acb92c775ef37c15a9016c",
+    "url": "/react-ts-flappy-bird/static/media/pipeNorth.07cd2335.png"
+  },
+  {
+    "revision": "f791429212203924c69e2f4096681cea",
+    "url": "/react-ts-flappy-bird/static/media/pipeSouth.f7914292.png"
+  }
+]);
